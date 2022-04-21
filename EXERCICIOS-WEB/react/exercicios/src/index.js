1ff85dc95640444eeabfb75b309c82de6b497795
claudioferreira <claudioferreirajr@hotmail.com>
2022-04-20 22:38:19 -0300

    conection with dom

---
+++ EXERCICIOS-WEB/react/exercicios/src/index.js
@@ -0,0 +1,5 @@
+import React from "react"; //core do framework
+import ReactDOM from "react-dom"; //parte do react que conversa com a pagina
+
+const elemento = <h1>React 2</h1>
+ReactDOM.render(elemento, document.getElementById('root'))
