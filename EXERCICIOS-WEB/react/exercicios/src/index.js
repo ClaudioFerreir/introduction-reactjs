52aab9ff6f596f883f7235b8e1874071daf021f9
claudioferreira <claudioferreirajr@hotmail.com>
2022-04-20 22:53:36 -0300

    first lesson

--- EXERCICIOS-WEB/react/exercicios/src/index.js
+++ EXERCICIOS-WEB/react/exercicios/src/index.js
@@ -1,5 +1,5 @@
-import React from "react"; //core do framework
 import ReactDOM from "react-dom"; //parte do react que conversa com a pagina
 
-const elemento = <h1>React 2</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro'
+
+ReactDOM.render(< Primeiro />, document.getElementById('root'))
